d05b5ada9b6fc7603f134342a04e03adbe55cf50
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2022-12-18 17:02:40 +0530

    Modified for Comparison Between Two Lines

--- LineComparisonProblemDay3/Program.cs
+++ LineComparisonProblemDay3/Program.cs
@@ -4,36 +4,40 @@
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Please enter X1 and Y1 coordinates");
+            var X1 = Convert.ToDouble(Console.ReadLine());
+            var Y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter X2 and Y2 coordinates");
+            var X2 = Convert.ToDouble(Console.ReadLine());
+            var Y2 = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Please enter X3 and Y3 coordinates");
             var X3 = Convert.ToDouble(Console.ReadLine());
             var Y3 = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Please enter X4 and Y4 coordinates");
             var X4 = Convert.ToDouble(Console.ReadLine());
             var Y4 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Please enter X5 and Y5 coordinates");
-            var X5 = Convert.ToDouble(Console.ReadLine());
-            var Y5 = Convert.ToDouble(Console.ReadLine());
-            Console.WriteLine("Please enter X6 and Y6 coordinates");
-            var X6 = Convert.ToDouble(Console.ReadLine());
-            var Y6 = Convert.ToDouble(Console.ReadLine());
 
-            double temp1 = Math.Pow((X4 - X3), 2);
-            double temp2 = Math.Pow((Y4 - Y3), 2);
-            double Length1 = Math.Sqrt(temp1 + temp2);
-            Console.WriteLine("Length of line1:" + Length1);
+            double temp1 = Math.Pow((X2 - X1), 2);
+            double temp2 = Math.Pow((Y2 - Y1), 2);
+            double LengthOfFirstLine = Math.Sqrt(temp1 + temp2);
 
-            double temp3 = Math.Pow((X6 - X5), 2);
-            double temp4 = Math.Pow((Y6 - Y5), 2);
-            double Length2 = Math.Sqrt(temp3 + temp4);
-            Console.WriteLine("Length of line2:" + Length2);
+            double temp3 = Math.Pow((X4 - X3), 2);
+            double temp4 = Math.Pow((Y4 - Y3), 2);
+            double LengthOfSecondLine = Math.Sqrt(temp3 + temp4);
+            Console.WriteLine("The length of Line1 {0}. \n And length of Line2 {1}:", LengthOfFirstLine, LengthOfSecondLine);
 
-            if (Length1 == Length2)
+            if (LengthOfFirstLine == LengthOfSecondLine)
             {
                 Console.WriteLine("Lenght of two lines are equal");
             }
+            else if (LengthOfFirstLine > LengthOfSecondLine)
+            {
+                Console.WriteLine("Lenght of line1 is greater than length of line2");
+            }
             else
             {
-                Console.WriteLine("Lenght of two lines are not equal");
+                Console.WriteLine("Length of line1 is smaller than length of line2");
             }
         }
     }
