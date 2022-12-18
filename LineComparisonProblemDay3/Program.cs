04f1a0d9d3a52885b3fac7da54b5c03de36ca120
ShivaniDivekar25 <shivanidivekar2@gmail.com>
2022-12-18 16:36:05 +0530

    Modified to calculate Length of Line

--- LineComparisonProblemDay3/Program.cs
+++ LineComparisonProblemDay3/Program.cs
@@ -5,6 +5,16 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcomw! to Line Comparison Pragram");
+            Console.WriteLine("Please enter X1 and Y1 coordinates");
+            var X1 = Convert.ToDouble(Console.ReadLine());
+            var Y1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter X2 and Y2 coordinates");
+            var X2 = Convert.ToDouble(Console.ReadLine());
+            var Y2 = Convert.ToDouble(Console.ReadLine());
+            double temp1 = Math.Pow((X2 - X1), 2);
+            double temp2 = Math.Pow((Y2 - Y1), 2);
+            double result = Math.Sqrt(temp1 + temp2);
+            Console.WriteLine("The length of line is:" + result);
         }
     }
 }
